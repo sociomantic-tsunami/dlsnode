nemanja-boric-sociomantic/dlsnode-1

Allow discarding scheduled AsyncIO requests
In case when StorageEngine no longer needs result of the asyncio
operation, now it can be discarded through the suspendable request that
was assigned to that job.

src/dlsnode/util/aio/SuspendableRequestHandler.d
@@ -16,6 +16,28 @@ module dlsnode.util.aio.SuspendableRequestHandler;
 /// Ditto
 abstract class SuspendableRequestHandler
 {
+    import ocean.core.array.Mutation: copy;
+    import dlsnode.util.aio.internal.JobQueue: Job;
+
+    /***************************************************************************
+
+        AsyncIO's method that should be called to discard the results of scheduled
+        asynchronous method.
+
+    ***************************************************************************/
+
+    private void delegate(Job*) remove_dg;
+
+    /***************************************************************************
+
+        Pointer to the AIO job suspended on this SuspendableRequestHandler.
+        Used for canceling the jobs. TODO: is the closure possible combining
+        remove_dg and job?
+
+    ***************************************************************************/
+
+    private Job* job;
+
     /***************************************************************************
 
         Yields the control to the suspendable request, indicating that the aio
@@ -39,10 +61,16 @@ abstract class SuspendableRequestHandler
         Cedes the control from the suspendable request, waiting for the aio
         operation to be done. Called by AsyncIO framework internally.
 
+        Params:
+            remove_dg = delegate to call to inform AIO that results of this operation
+                        can be discarded.
+
     ***************************************************************************/
 
-    public final void wait ()
+    public final void wait (Job* job, typeof(this.remove_dg) remove_dg)
     {
+        this.remove_dg = remove_dg;
+        this.job = job;
         this.wait_();
     }
 
@@ -56,5 +84,21 @@ abstract class SuspendableRequestHandler
     public final void wake ()
     {
         this.wake_();
+    }
+
+    /***************************************************************************
+
+        Should be called by the concrete classes to inform the AIO that
+        results of this operation are no longer needed.
+
+    ***************************************************************************/
+
+    public final void discardResults ()
+    {
+        if (this.job)
+        {
+            this.remove_dg(this.job);
+            this.job = null;
+        }
     }
 }

sociomantic-tsunami/dlsnode

src/dlsnode/util/aio/SuspendableRequestHandler.d
@@ -16,6 +16,28 @@ module dlsnode.util.aio.SuspendableRequestHandler;
 /// Ditto
 abstract class SuspendableRequestHandler
 {
+    import ocean.core.array.Mutation: copy;
+    import dlsnode.util.aio.internal.JobQueue: Job;
+
+    /***************************************************************************
+
+        AsyncIO's method that should be called to discard the results of scheduled
+        asynchronous method.
+
+    ***************************************************************************/
+
+    private void delegate(Job*) remove_dg;
+
+    /***************************************************************************
+
+        Pointer to the AIO job suspended on this SuspendableRequestHandler.
+        Used for canceling the jobs. TODO: is the closure possible combining
+        remove_dg and job?
+
+    ***************************************************************************/
+
+    private Job* job;
+
     /***************************************************************************
 
         Yields the control to the suspendable request, indicating that the aio
@@ -39,10 +61,16 @@ abstract class SuspendableRequestHandler
         Cedes the control from the suspendable request, waiting for the aio
         operation to be done. Called by AsyncIO framework internally.
 
+        Params:
+            remove_dg = delegate to call to inform AIO that results of this job
+                        can be discarded.
+
     ***************************************************************************/
 
-    public final void wait ()
+    public final void wait (Job* job, typeof(this.remove_dg) remove_dg)
     {
+        this.remove_dg = remove_dg;
+        this.job = job;
         this.wait_();
     }
 
@@ -56,5 +84,21 @@ abstract class SuspendableRequestHandler
     public final void wake ()
     {
         this.wake_();
+    }
+
+    /***************************************************************************
+
+        Should be called by the concrete classes to inform the AIO that
+        results of this operation are no longer needed.
+
+    ***************************************************************************/
+
+    public final void discardResults ()
+    {
+        if (this.job)
+        {
+            this.remove_dg(this.job);
+            this.job = null;
+        }
     }
 }